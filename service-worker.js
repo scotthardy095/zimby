8f7efce42a20150c4e4b28ff14e54cd395993479
scotthardy095 <scotthardy095@gmail.com>
2025-09-05 21:43:04 +0100

    Replace binary icons with inline SVG data URIs

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'zimby-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  console.log('Service worker installed');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => response || fetch(event.request))
+  );
+});
+
